da9931309e3c5e9d878b3c01967add8ddd3bc29e
Kinnari <kkshah209@gmail.com>
2024-04-13 14:25:49 +0530

    Added Navbar component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,15 @@ import './App.css'
 function App() {
   return (
     <>
+          <div className="navbar">  {/* Add class for CSS styling */}
+        <div className="nav-content">
+          <a className="nav-link" href="#">Dashboard</a>
+          <a className="nav-link" href="#">Learn</a>
+          {/* <a className="nav-link" href="#">Statistics</a>
+          <a className="nav-link" href="#">Courses</a>
+          <a className="nav-link" href="#">Settings</a> */}
+        </div>
+      </div>
     </>
   )
 }
